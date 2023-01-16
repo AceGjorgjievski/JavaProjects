c2daf4dfc231a83926d3e450ddba0728824ad3da
AcePNG <ace.gjorgjievski6@gmail.com>
2023-01-16 12:17:09 +0100

    Added text to the task

--- src/mk/ukim/finki/np/juni/vlez/EntryTask.java
+++ src/mk/ukim/finki/np/juni/vlez/EntryTask.java
@@ -5,6 +5,31 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
+/**
+ * Влезна задача:
+ * текстот беше нешто да се најде максималниот број на појавувања на даден карактер
+ * во дадена линија и да се испечати тој број.
+ *
+ * нешто отприлика вака е влезот:
+ *
+ * input
+ * -------
+ * zzzdravoo na site
+ * prijaaatno
+ * naprrredno programiranje
+ *
+ * CTRL + D za da imate output na console posle odreden input
+ *
+ * output
+ * ----------
+ * 3 (бидејќи 'z' 3 пати се појавува)
+ * 3 (бидејќи 'a' 3 пати се појавува)
+ * 6 (бидејќи 'r' 6 пати се појавува)
+ *
+ * Решение:
+ */
+
 class Line {
     Map<Character, Integer> map;
 
@@ -57,6 +82,10 @@ class TextProcessor {
 
     }
 }
+
+/**
+ *
+ */
 
 
 public class EntryTask {
